3b6d084c4ba4b3565bc712690003fc8df3e530b5
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-09-17 19:42:30 +0300

    conditionals

---
+++ src/Conditionals/Main.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 27;
+
+        if(sayi<20){
+            System.out.println("sayi 20'den kucuk");
+        }else if (sayi == 20){
+            System.out.println("sayi 20ye eşittir");
+        }else {
+            System.out.println("sayi 20 den buyuktur");
+        }
+
+    }
+}
---
+++ src/DataTypes/Main.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi = 12;
+        double sayi2= 20.23;
+        byte sayi3 = 127;
+        float sayi4 = 1.29450f;
+        boolean dogruMu= false;
+        char karakter = 'a';
+
+        System.out.println(sayi + " " + sayi2 + " " + sayi3 + " " + sayi4 + " " + dogruMu + " " + karakter);
+
+    }
+}
